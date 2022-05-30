3509678b2144450ebb10618064c042a6b989c9d4
Edmund <edmund.adjei635@gmail.com>
2022-05-30 17:20:25 -0400

    Changes to all

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Brooklyn Nets Fan Page";
 document.querySelector("body").appendChild(h2);
+const table = document.getElementById("contact info");
